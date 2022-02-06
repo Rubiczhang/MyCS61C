8eb39ee1dfb2b0d20973cc931c1dac0e6fbdf5e8
Rubic <1411230706@qq.com>
2022-02-06 16:27:20 +0800

    lab5 done

---
+++ proj2/unittests/assembly/TestArgmax_test_invalid_n.s
@@ -0,0 +1,19 @@
+.import ../../src/utils.s
+.import ../../src/argmax.s
+
+.data
+
+.globl main_test
+.text
+# main_test function for testing
+main_test:
+
+    # load 0 into a1
+    li a1 0
+
+    # call argmax function
+    jal ra argmax
+    # we expect argmax to exit early with code 57
+
+    # exit normally
+    jal exit
